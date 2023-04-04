d2587aefd4d6ffc78f17f3a9c41f38d55da1aecd
Frycaur <103007112+Frycaur@users.noreply.github.com>
2023-04-04 21:42:27 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-const popUpBtn = document.getElementById('pop-up');
-const popUpMsg = document.getElementById('pop-up-message');
-const closeBtn = document.getElementById('close');
+// Get the pop-up message element
+const popUpMessage = document.getElementById("pop-up-message");
 
-popUpBtn.addEventListener('click', function() {
-	popUpMsg.style.display = 'flex';
+// Get the button to open the pop-up message
+const popUpButton = document.getElementById("pop-up");
+
+// Get the button to close the pop-up message
+const closeButton = document.getElementById("close");
+
+// When the user clicks the button, show the pop-up message
+popUpButton.addEventListener("click", function() {
+	popUpMessage.style.display = "block";
 });
 
-closeBtn.addEventListener('click', function() {
-	popUpMsg.style.display = 'none';
+// When the user clicks the close button, hide the pop-up message
+closeButton.addEventListener("click", function() {
+	popUpMessage.style.display = "none";
 });
